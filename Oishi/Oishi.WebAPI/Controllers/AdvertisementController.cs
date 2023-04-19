ce3c5587343f24f2fc5e0bfad00414d877dbff9d
Sonia Rodrigues <soniaalexandraa94@gmail.com>
2023-04-19 20:02:56 +0100

    Layout
    Do a general layout and add js, css, libs jquery, bootstrap...

--- Oishi/Oishi.WebAPI/Controllers/AdvertisementController.cs
+++ Oishi/Oishi.WebAPI/Controllers/AdvertisementController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Oishi.Data.Models;
 
 namespace Oishi.WebAPI.Controllers
---
+++ Oishi/Oishi.WebApp/Models/RegisterViewModel.cs
@@ -0,0 +1,9 @@
+namespace Oishi.WebApp.Models
+{
+    public class RegisterViewModel
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
