af2eae6e0a3cdb1500585df187449b6b108a9194
Sonia Rodrigues <soniaalexandraa94@gmail.com>
2023-04-23 22:30:04 +0100

    Menu Categories Index
    Add icons and create menu base

---
+++ Oishi/Oishi.WebApp/wwwroot/js/index.js
@@ -0,0 +1,13 @@
+﻿
+function HideSubCategories(current_id) {
+    $('.sub-category-list-item').not('.' + current_id).hide();
+}
+
+function ToggleSubCategory(item) {
+    HideSubCategories(item.id);
+    $('.' + item.id).toggle();
+}
+
+$(document).ready(function () {
+    HideSubCategories('all')
+});
