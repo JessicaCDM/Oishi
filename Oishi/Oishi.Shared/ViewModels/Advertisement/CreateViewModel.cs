02eda3a9fbc3c7e34a5a34a5b95b1eb677f42ad8
laurasacosta <lauramdsc@gmail.com>
2023-05-04 01:13:39 +0100

    backup e edit adivertisement

---
+++ Oishi/Oishi.Shared/ViewModels/Advertisement/CreateViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oishi.Shared.ViewModels.Advertisement
+{
+    public class CreateViewModel
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Insira título")]
+        [Required(ErrorMessage = "Insira um título")]
+        [StringLength(64)]
+        public string Title { get; set; }
+
+        [DisplayName("Insira uma descrição")]
+        [Required(ErrorMessage = "Insira uma descrição")]
+        [StringLength(1000)]
+        public string Description { get; set; }
+
+        [DisplayName("Insira um preço")]
+        [Required(ErrorMessage = "Insira um preço")]
+        [Column(TypeName = "decimal(7,2)")]
+        public decimal Price { get; set; }
+        public int UserAccountId { get; set; }
+        public int MunicipalityOrCityId { get; set; }
+        public int SubcategoryId { get; set; }
+    }
+}
--- Oishi/Oishi.WebApp/Controllers/AdvertisementController.cs
+++ Oishi/Oishi.WebApp/Controllers/AdvertisementController.cs
@@ -1,16 +1,86 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Construction;
+using Newtonsoft.Json;
+using Oishi.Shared.ViewModels.Account;
+using Oishi.Shared.ViewModels.Advertisement;
+using System.Configuration;
+using System.Reflection;
 
 namespace Oishi.WebApp.Controllers
 {
     public class AdvertisementController : Controller
     {
+        private string _OishiWebApiAddress;
+        public AdvertisementController(IConfiguration configuration)
+        {
+            _OishiWebApiAddress = configuration.GetValue<string>("OishiWebApiAddress");
+        }
         public IActionResult Index()
         {
             return View();
         }
+
+        // GET Advertisement/Create
         public IActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateViewModel model)
+        {
+            using (Oishi.Shared.Providers.WebAPIProvider webAPIProvider = new Shared.Providers.WebAPIProvider(_OishiWebApiAddress))
+            {
+                string? response = await webAPIProvider.Post($"Advertisement/Insert", model);
+                System.Diagnostics.Debug.WriteLine(response);
+
+                ViewData["Success"] = "Anúncio criado com sucesso!";
+            }
+                        
+            return View();
+        }
+
+        // GET Advertisement/Update?Id=123
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            CreateViewModel? model = null;
+            using (Oishi.Shared.Providers.WebAPIProvider webAPIProvider = new Shared.Providers.WebAPIProvider(_OishiWebApiAddress))
+            {
+                string? response = await webAPIProvider.Get($"Advertisement/GetFirst?id={id}");
+                if (response != null)
+                    model = JsonConvert.DeserializeObject<CreateViewModel>(response);
+            }
+            ViewData["EditView"] = true;
+
+            if (model == null)
+            {
+                throw new Exception("Houve um erro");
+            }
+            return View("Create", model);
+        }
+
+        // POST Advertisement/Update?Id=123
+        [HttpPost]
+
+        public async Task<IActionResult> Edit(CreateViewModel model)
+        {
+            using (Oishi.Shared.Providers.WebAPIProvider webAPIProvider = new Shared.Providers.WebAPIProvider(_OishiWebApiAddress))
+            {
+                string? response = await webAPIProvider.Post($"Advertisement/Update", model);
+                System.Diagnostics.Debug.WriteLine(response);
+
+                ViewData["Success"] = "Anúncio editado com sucesso!";
+            }
+
+            return View();
+        }
+
+
+
+
+
+
+
     }
 }
