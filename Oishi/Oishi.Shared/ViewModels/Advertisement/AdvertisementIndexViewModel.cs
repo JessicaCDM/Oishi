cfbf305dc476a8ba60352c8e117e41166b271e44
Sonia Rodrigues <soniaalexandraa94@gmail.com>
2023-05-04 20:35:14 +0100

    Creation of the needed view models for the Advertisement Index view

---
+++ Oishi/Oishi.Shared/ViewModels/Advertisement/AdvertisementIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oishi.Shared.ViewModels.Advertisement
+{
+    public class AdvertisementIndexViewModel
+    {
+        public Oishi.Shared.ViewModels.Category.CategoryViewModel[] Categories { get; set; }
+        public Oishi.Shared.ViewModels.Advertisement.AdvertisementViewModel[] Advertisements{ get; set; }
+
+    }
+}
---
+++ Oishi/Oishi.Shared/ViewModels/Advertisement/AdvertisementViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oishi.Shared.ViewModels.Category;
+
+namespace Oishi.Shared.ViewModels.Advertisement
+{
+    public class AdvertisementViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime StartDate { get; set; }
+        public decimal Price { get; set; }
+        public string SubCategoryDescription { get; set; }
+        public string MunicipalityOrCityName { get; set; }
+    }
+}
