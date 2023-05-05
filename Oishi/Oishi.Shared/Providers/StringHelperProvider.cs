05355222d84f81dca97fb614dba55c4f0fbfd817
Sonia Rodrigues <soniaalexandraa94@gmail.com>
2023-05-05 11:48:19 +0100

    New function to remove spaces and special characters from string

---
+++ Oishi/Oishi.Shared/Providers/StringHelperProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oishi.Shared.Providers
+{
+    public static class StringHelperProvider
+    {
+        public static string RemoveDiacriticsAndSpaces(string text)
+        {
+            var newString = text.Replace(" ", "");
+            newString = newString.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder(capacity: newString.Length);
+
+            for (int i = 0; i < newString.Length; i++)
+            {
+                char c = newString[i];
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder
+                .ToString()
+                .Normalize(NormalizationForm.FormC);
+        }
+    }
+}
