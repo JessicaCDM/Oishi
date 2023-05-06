3aaad72c931d1b25373780b5e908d28c78fa2d62
Sonia Rodrigues <soniaalexandraa94@gmail.com>
2023-05-06 17:15:36 +0100

    Changes in the Advertisement Controller related to Authentication

--- Oishi/Oishi.WebApp/Controllers/AdvertisementController.cs
+++ Oishi/Oishi.WebApp/Controllers/AdvertisementController.cs
@@ -1,112 +1,114 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Security.Claims;
 
 namespace Oishi.WebApp.Controllers
 {
-    public class AdvertisementController : Controller
-    {
-        private string _OishiWebApiAddress;
+	public class AdvertisementController : Controller
+	{
+		private string _OishiWebApiAddress;
 
-        public AdvertisementController(IConfiguration configuration)
-        {
-            _OishiWebApiAddress = configuration.GetValue<string>("OishiWebApiAddress");
-        }
+		public AdvertisementController(IConfiguration configuration)
+		{
+			_OishiWebApiAddress = configuration.GetValue<string>("OishiWebApiAddress");
+		}
 
-        public async Task<IActionResult> Index(int? id)
-        {
-            Shared.ViewModels.Advertisement.AdvertisementIndexViewModel model = new Shared.ViewModels.Advertisement.AdvertisementIndexViewModel();
+		public async Task<IActionResult> Index(int? id)
+		{
+			Shared.ViewModels.Advertisement.AdvertisementIndexViewModel model = new Shared.ViewModels.Advertisement.AdvertisementIndexViewModel();
 
-            Shared.ViewModels.Category.CategoryViewModel[] categories = null;
-            using (Oishi.Shared.Providers.WebAPIProvider webAPIProvider = new Shared.Providers.WebAPIProvider(_OishiWebApiAddress))
-            {
-                string? apiResponse = await webAPIProvider.Get($"Category/Get");
-                if (apiResponse != null)
-                    categories = JsonConvert.DeserializeObject<Shared.ViewModels.Category.CategoryViewModel[]>(apiResponse);
-            }
+			Shared.ViewModels.Category.CategoryViewModel[] categories = null;
+			using (Oishi.Shared.Providers.WebAPIProvider webAPIProvider = new Shared.Providers.WebAPIProvider(_OishiWebApiAddress))
+			{
+				string? apiResponse = await webAPIProvider.Get($"Category/Get");
+				if (apiResponse != null)
+					categories = JsonConvert.DeserializeObject<Shared.ViewModels.Category.CategoryViewModel[]>(apiResponse);
+			}
 
-            Shared.ViewModels.Advertisement.AdvertisementViewModel[] advertisements = null;
-            using (Oishi.Shared.Providers.WebAPIProvider webAPIProvider = new Shared.Providers.WebAPIProvider(_OishiWebApiAddress))
-            {
-                string? apiResponse;
-                if (id.HasValue)
-                {
-                    // TODO: Replace '3' with the real UserAccountId
-                    apiResponse = await webAPIProvider.Get($"Advertisement/GetFiltered?subCategoryId={id}&favoriteUserAccountId={3}");
-                }
-                else
-                {
-                    // TODO: Replace '3' with the real UserAccountId
-                    apiResponse = await webAPIProvider.Get($"Advertisement/GetFiltered?favoriteUserAccountId={3}");
-                }
+			Shared.ViewModels.Advertisement.AdvertisementViewModel[] advertisements = null;
+			using (Oishi.Shared.Providers.WebAPIProvider webAPIProvider = new Shared.Providers.WebAPIProvider(_OishiWebApiAddress))
+			{
+				string? apiResponse;
 
-                if (apiResponse != null)
-                    advertisements = JsonConvert.DeserializeObject<Shared.ViewModels.Advertisement.AdvertisementViewModel[]>(apiResponse);
-            }
+				// Get User ID if login is done
+				int.TryParse(User.FindFirstValue(ClaimTypes.NameIdentifier), out int userId);
 
-            if (categories != null && advertisements != null)
-            {
-                model.Categories = categories;
-                model.Advertisements = advertisements;
-                return View(model);
-            }
+				Shared.ViewModels.Advertisement.AdvertisementSearchViewModel searchModel = new Shared.ViewModels.Advertisement.AdvertisementSearchViewModel()
+				{
+					FavoriteUserAccountId = userId,
+					SubCategoryId = id
+				};
 
-            throw new Exception();
-        }
+				apiResponse = await webAPIProvider.Post($"Advertisement/GetFiltered", searchModel);
 
-        // GET Advertisement/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+				if (apiResponse != null)
+					advertisements = JsonConvert.DeserializeObject<Shared.ViewModels.Advertisement.AdvertisementViewModel[]>(apiResponse);
+			}
 
-        // POST Advertisement/Create
-        [HttpPost]
-        public async Task<IActionResult> Create(Shared.ViewModels.Advertisement.CreateViewModel model)
-        {
-            using (Oishi.Shared.Providers.WebAPIProvider webAPIProvider = new Shared.Providers.WebAPIProvider(_OishiWebApiAddress))
-            {
-                string? response = await webAPIProvider.Post($"Advertisement/Insert", model);
-                System.Diagnostics.Debug.WriteLine(response);
+			if (categories != null && advertisements != null)
+			{
+				model.Categories = categories;
+				model.Advertisements = advertisements;
+				return View(model);
+			}
 
-                ViewData["Success"] = "Anúncio criado com sucesso!";
-            }
+			throw new Exception();
+		}
 
-            return View();
-        }
+		// GET Advertisement/Create
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-        // GET Advertisement/Edit?Id=123
+		// POST Advertisement/Create
+		[HttpPost]
+		public async Task<IActionResult> Create(Shared.ViewModels.Advertisement.CreateViewModel model)
+		{
+			using (Oishi.Shared.Providers.WebAPIProvider webAPIProvider = new Shared.Providers.WebAPIProvider(_OishiWebApiAddress))
+			{
+				string? response = await webAPIProvider.Post($"Advertisement/Insert", model);
+				System.Diagnostics.Debug.WriteLine(response);
 
-        public async Task<IActionResult> Edit(int id)
-        {
-            Shared.ViewModels.Advertisement.CreateViewModel? model = null;
-            using (Oishi.Shared.Providers.WebAPIProvider webAPIProvider = new Shared.Providers.WebAPIProvider(_OishiWebApiAddress))
-            {
-                string? response = await webAPIProvider.Get($"Advertisement/GetFirst?id={id}");
-                if (response != null)
-                    model = JsonConvert.DeserializeObject<Shared.ViewModels.Advertisement.CreateViewModel>(response);
-            }
-            ViewData["EditView"] = true;
+				ViewData["Success"] = "Anúncio criado com sucesso!";
+			}
 
-            if (model == null)
-            {
-                throw new Exception("Houve um erro");
-            }
-            return View("Create", model);
-        }
+			return View();
+		}
 
-        // POST Advertisement/Edit?Id=123
-        [HttpPost]
-        public async Task<IActionResult> Edit(Shared.ViewModels.Advertisement.CreateViewModel model)
-        {
-            using (Oishi.Shared.Providers.WebAPIProvider webAPIProvider = new Shared.Providers.WebAPIProvider(_OishiWebApiAddress))
-            {
-                string? response = await webAPIProvider.Post($"Advertisement/Update", model);
-                System.Diagnostics.Debug.WriteLine(response);
+		// GET Advertisement/Edit?Id=123
 
-                ViewData["Success"] = "Anúncio editado com sucesso!";
-            }
+		public async Task<IActionResult> Edit(int id)
+		{
+			Shared.ViewModels.Advertisement.CreateViewModel? model = null;
+			using (Oishi.Shared.Providers.WebAPIProvider webAPIProvider = new Shared.Providers.WebAPIProvider(_OishiWebApiAddress))
+			{
+				string? response = await webAPIProvider.Get($"Advertisement/GetFirst?id={id}");
+				if (response != null)
+					model = JsonConvert.DeserializeObject<Shared.ViewModels.Advertisement.CreateViewModel>(response);
+			}
+			ViewData["EditView"] = true;
 
-            return View();
-        }
-    }
+			if (model == null)
+			{
+				throw new Exception("Houve um erro");
+			}
+			return View("Create", model);
+		}
+
+		// POST Advertisement/Edit?Id=123
+		[HttpPost]
+		public async Task<IActionResult> Edit(Shared.ViewModels.Advertisement.CreateViewModel model)
+		{
+			using (Oishi.Shared.Providers.WebAPIProvider webAPIProvider = new Shared.Providers.WebAPIProvider(_OishiWebApiAddress))
+			{
+				string? response = await webAPIProvider.Post($"Advertisement/Update", model);
+				System.Diagnostics.Debug.WriteLine(response);
+
+				ViewData["Success"] = "Anúncio editado com sucesso!";
+			}
+
+			return View();
+		}
+	}
 }
