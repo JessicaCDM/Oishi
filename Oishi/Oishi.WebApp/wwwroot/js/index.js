0b67d12b913f4c4b7952cec4cd67892e2029fa20
Sonia Rodrigues <soniaalexandraa94@gmail.com>
2023-05-08 18:23:35 +0100

    Added function favorite in js index homepage

--- Oishi/Oishi.WebApp/wwwroot/js/index.js
+++ Oishi/Oishi.WebApp/wwwroot/js/index.js
@@ -15,3 +15,21 @@ function ToggleSubCategory(id) {
 $(document).ready(function () {
     HideSubCategories('all')
 });
+
+function ToggleFavorite(id) {
+    $.ajax({
+        url: window.location.origin + '/Favorite/Toggle/' + id,
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            if (data == true) {
+                $('#advertisement' + id + 'favicon').removeClass('far').addClass('fa-solid')
+            } else {
+                $('#advertisement' + id + 'favicon').removeClass('fa-solid').addClass('far')
+            }
+        },
+        error: function (request, error) {
+            alert("Request: " + JSON.stringify(request));
+        }
+    });
+}
